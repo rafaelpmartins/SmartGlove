8ff9db0ecb9288549c2ada9fb2a4dc1b0593ac27
rafaelmartins <Rafael.martins92@etec.sp.gov.br>
2019-08-03 21:38:52 -0300

    tela cadastro design completo

--- app/src/main/java/com/example/smartglove/Cadastro_Activity.java
+++ app/src/main/java/com/example/smartglove/Cadastro_Activity.java
@@ -1,9 +1,13 @@
 package com.example.smartglove;
 
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
 
@@ -17,11 +21,35 @@ public class Cadastro_Activity extends AppCompatActivity implements DatePickerDi
 
     private TextView dataText;
     private TextView esporteText;
+    private TextView irLogin;
+    private Button btnCadastrar;
+    String[] listItems;
+    boolean[] checkedItems;
+    ArrayList<Integer> mUserItems = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cadastro_layout);
+
+        irLogin = (TextView) findViewById(R.id.irLogin);
+        btnCadastrar = (Button) findViewById(R.id.btnCadastrar);
+
+        btnCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        irLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Login_Activity.class);
+                startActivity(intent);
+            }
+        });
 
         dataText = (TextView) findViewById(R.id.dataText);
         dataText.setOnClickListener(new View.OnClickListener() {
@@ -31,7 +59,67 @@ public class Cadastro_Activity extends AppCompatActivity implements DatePickerDi
             }
         });
 
-        esporteText = (TextView)findViewById(R.id.esporteText);
+        esporteText = (TextView) findViewById(R.id.esporteText);
+
+        listItems = getResources().getStringArray(R.array.esporte_item);
+        checkedItems = new boolean[listItems.length];
+
+        esporteText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(Cadastro_Activity.this);
+                mBuilder.setTitle(R.string.dialog_title);
+                mBuilder.setMultiChoiceItems(listItems, checkedItems, new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int position, boolean isChecked) {
+                        if (isChecked) {
+                            mUserItems.add(position);
+                        } else {
+                            mUserItems.remove((Integer.valueOf(position)));
+                        }
+                    }
+                });
+
+                mBuilder.setCancelable(false);
+                mBuilder.setPositiveButton(R.string.ok_label, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        String item = "";
+                        for (int i = 0; i < mUserItems.size(); i++) {
+                            item = item + listItems[mUserItems.get(i)];
+                            if (i != mUserItems.size() - 1) {
+                                item = item + ", ";
+                            }
+                        }
+                        esporteText.setText(item);
+                        if (esporteText.getText() == "") {
+                            esporteText.setText("Adicionar Esportes");
+                        }
+                    }
+                });
+
+                mBuilder.setNegativeButton(R.string.dismiss_label, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+
+                mBuilder.setNeutralButton(R.string.clear_all_label, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        for (int i = 0; i < checkedItems.length; i++) {
+                            checkedItems[i] = false;
+                            mUserItems.clear();
+                            esporteText.setText("Adicionar Esportes");
+                        }
+                    }
+                });
+
+                AlertDialog mDialog = mBuilder.create();
+                mDialog.show();
+            }
+        });
     }
 
     public void showDatePickerDialog() {
