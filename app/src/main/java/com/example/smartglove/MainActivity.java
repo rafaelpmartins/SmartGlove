9458993d587f4406befecb50caef0e4349c0032d
rafaelmartins <Rafael.martins92@etec.sp.gov.br>
2019-08-05 16:47:32 -0300

    adicionei menu na activity_main

--- app/src/main/java/com/example/smartglove/MainActivity.java
+++ app/src/main/java/com/example/smartglove/MainActivity.java
@@ -1,13 +1,74 @@
 package com.example.smartglove;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Smart Glove");
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView = (NavigationView) findViewById(R.id.navView);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_item_one: {
+                Toast.makeText(this, "Menu 1", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.nav_item_two: {
+                Toast.makeText(this, "Menu 2", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.nav_item_three: {
+                Toast.makeText(this, "Menu 3", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.nav_item_four: {
+                Toast.makeText(this, "Menu 4", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            default: {
+                Toast.makeText(this, "Menu Default", Toast.LENGTH_SHORT).show();
+                break;
+            }
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
