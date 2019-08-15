cfefbcb48ba1d4e26f80bda963bb96818fc992c0
rafaelmartins <Rafael.martins92@etec.sp.gov.br>
2019-08-15 16:41:39 -0300

    Visibilidade do bluetooth funcionando

--- app/src/main/java/com/example/smartglove/Bluetooth_Activity.java
+++ app/src/main/java/com/example/smartglove/Bluetooth_Activity.java
@@ -1,7 +1,6 @@
 package com.example.smartglove;
 
 import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -15,10 +14,12 @@ import android.widget.Toast;
 
 public class Bluetooth_Activity extends AppCompatActivity {
 
-    Button btnConectar;
+    Button btnLigar;
+    Button btnVisibilidadeBT;
     BluetoothAdapter mBluetoothAdapter;
     TextView estadoBluetooth;
     TextView estadoBluetoothTitulo;
+    TextView VisibilidadeBluetooh;
 
     //BT = bluetooth
 
@@ -30,16 +31,27 @@ public class Bluetooth_Activity extends AppCompatActivity {
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         estadoBluetoothTitulo = (TextView) findViewById(R.id.estadoBluetoohTitulo);
         estadoBluetooth = (TextView) findViewById(R.id.estadoBluetooh);
+        btnLigar = (Button) findViewById(R.id.btnLigar);
+        btnVisibilidadeBT = (Button) findViewById(R.id.btnVisibilidadeBT);
+        VisibilidadeBluetooh = (TextView) findViewById(R.id.VisibilidadeBluetooh);
 
-        btnConectar = (Button) findViewById(R.id.btnConectar);
-        btnConectar.setOnClickListener(new View.OnClickListener() {
+        btnLigar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 ligarDesligarBluetooth();
+                btnVisibilidadeBT.setVisibility(View.VISIBLE);
+            }
+        });
+
+        btnVisibilidadeBT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                VisivelBT();
             }
         });
     }
 
+    //serve para checar o estado do bluetooh
     private final BroadcastReceiver mBroadcastReceiver1 = new BroadcastReceiver() {
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
@@ -64,9 +76,37 @@ public class Bluetooth_Activity extends AppCompatActivity {
             }
         }
     };
+    //serve para checar a visibilidade do bluetooth
+    private final BroadcastReceiver mBroadcastReceiver2 = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (action.equals(mBluetoothAdapter.ACTION_SCAN_MODE_CHANGED)) {
+                int modo = intent.getIntExtra(BluetoothAdapter.EXTRA_SCAN_MODE, BluetoothAdapter.ERROR);
+
+                switch (modo) {
+                    case BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE:
+                        VisibilidadeBluetooh.setText("Visivel");
+                        break;
+                    case BluetoothAdapter.SCAN_MODE_CONNECTABLE:
+                        VisibilidadeBluetooh.setText("Invisivel, e aberto a conexões");
+                        break;
+                    case BluetoothAdapter.SCAN_MODE_NONE:
+                        VisibilidadeBluetooh.setText("Invisivel, fechado a conexão");
+                        break;
+                    case BluetoothAdapter.STATE_CONNECTING:
+                        VisibilidadeBluetooh.setText("Conectando...");
+                        break;
+                    case BluetoothAdapter.STATE_CONNECTED:
+                        VisibilidadeBluetooh.setText("Conectado");
+                        break;
+                }
+            }
+        }
+    };
 
     @Override
     protected void onDestroy() {
+        mBluetoothAdapter.disable();
         Toast.makeText(this, "A Conexão se perdeu!", Toast.LENGTH_SHORT).show();
         super.onDestroy();
     }
@@ -76,7 +116,7 @@ public class Bluetooth_Activity extends AppCompatActivity {
             Toast.makeText(this, "Bluetooth não funciona no seu aparelho", Toast.LENGTH_SHORT).show();
         }
         if (!mBluetoothAdapter.isEnabled()) {
-            btnConectar.setText("Desligar");
+            btnLigar.setText("Desligar");
 
             Intent ligarBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivity(ligarBtIntent);
@@ -85,12 +125,23 @@ public class Bluetooth_Activity extends AppCompatActivity {
             registerReceiver(mBroadcastReceiver1, BluetoothIntent);
         }
         if (mBluetoothAdapter.isEnabled()) {
-            btnConectar.setText("Ligar");
+            btnLigar.setText("Ligar");
 
             mBluetoothAdapter.disable();
 
             IntentFilter BluetoothIntent = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
             registerReceiver(mBroadcastReceiver1, BluetoothIntent);
         }
+    }
+
+    private void VisivelBT() {
+        VisibilidadeBluetooh.setText("Visivel por 300 segundos...");
+
+        Intent VisibilidadeIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        VisibilidadeIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+        startActivity(VisibilidadeIntent);
+
+        IntentFilter VisibilidadeIntentFilter = new IntentFilter(mBluetoothAdapter.ACTION_SCAN_MODE_CHANGED);
+        registerReceiver(mBroadcastReceiver2, VisibilidadeIntentFilter);
     }
 }
