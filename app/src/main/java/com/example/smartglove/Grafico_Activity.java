8edf59c66e2e8fd40f977927af4a9a054c127cbd
rafaelmartins <Rafael.martins92@etec.sp.gov.br>
2019-09-04 14:17:32 -0300

    adicionei outra linha no grafico

--- app/src/main/java/com/example/smartglove/Grafico_Activity.java
+++ app/src/main/java/com/example/smartglove/Grafico_Activity.java
@@ -20,8 +20,11 @@ public class Grafico_Activity extends AppCompatActivity {
     //eixo X
     String[] axisData = {"Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"};
 
-    //eixo Y
+    //Força valores
     double[] yAxisData = {50.7, 20.5, 15.9, 30.1, 20.2, 60.6, 15.5, 40.4, 45.3, 10.1, 18.8, 90.9};
+
+    //Velocidade valores
+    double[] zAxisData = {10, 30, 20, 40, 60, 40, 80, 25, 35, 60, 90, 70};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,11 +35,13 @@ public class Grafico_Activity extends AppCompatActivity {
 
         List yAxisValues = new ArrayList();
         List axisValues = new ArrayList();
+        List zAxisValues = new ArrayList();
 
-        //valores do eixo Y e Cor da linha
-        Line line = new Line(yAxisValues).setColor(Color.parseColor("#B71C1C"));
+        //linha 1 e 2
+        Line line = new Line(yAxisValues).setColor(Color.parseColor("#9C27B0"));//roxo Força
+        Line line2 = new Line(zAxisValues).setColor(Color.parseColor("#00BFFF"));// azul velocidade
 
-        //adiciona dados no eixo x e y
+        //adiciona valores percorrendo o vetor
         for (int i = 0; i < axisData.length; i++) {
             axisValues.add(i, new AxisValue(i).setLabel(axisData[i]));
         }
@@ -45,32 +50,39 @@ public class Grafico_Activity extends AppCompatActivity {
             yAxisValues.add(new PointValue(i, (float) yAxisData[i]));
         }
 
-        //um list do tipo linha
+        for (int i = 0; i < zAxisData.length; i++) {
+            zAxisValues.add(new PointValue(i, (float) zAxisData[i]));
+        }
+
+        //declara as linhas como list
         List lines = new ArrayList();
         lines.add(line);
+        lines.add(line2);
 
-        //adicionando a linha ao grafico
+        //adicionando as linhas ao grafico
         LineChartData data = new LineChartData();
         data.setLines(lines);
 
         //definiu local, tamanho(texto) e cor do eixo x
         Axis axis = new Axis();
         axis.setValues(axisValues);
-        axis.setTextSize(16);
         axis.setTextColor(Color.parseColor("#B71C1C"));
+        //axis.setTextSize(16);
+        //axis.setName("Velocidade");
         data.setAxisXBottom(axis);
 
         //definiu local, tamanho(texto), cor e titulo do eixo y
         Axis yAxis = new Axis();
-        yAxis.setName("Força");
         yAxis.setTextColor(Color.parseColor("#B71C1C"));
-        yAxis.setTextSize(16);
+        //yAxis.setName("Força");
+        //yAxis.setTextSize(16);
         data.setAxisYLeft(yAxis);
 
-        //expecificou um teto para o eixo y
+        //expecificou um teto e um chão para o eixo y
         lineChartView.setLineChartData(data);
         Viewport viewport = new Viewport(lineChartView.getMaximumViewport());
         viewport.top = 110;
+        viewport.bottom = 0;
         lineChartView.setMaximumViewport(viewport);
         lineChartView.setCurrentViewport(viewport);
     }
